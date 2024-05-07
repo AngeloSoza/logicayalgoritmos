a1f07bde11e31cd039e188921c317d5397db58c6
AngeloSoza <angelosoza@uamv.edu.ni>
2024-05-07 11:29:40 -0600

    Ejercicios clase del dia 07/05/24

---
+++ output/Sesion300424/EjemploFor4.cpp
@@ -0,0 +1,7 @@
+//Guardar una matriz 3x3 y mostrarla
+#include <iostream>
+using namespace std;
+
+#define MAX 3
+
+int matriz [MAX]
---
+++ output/Sesion300424/Sesion070524.cpp/ejercicio2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*Contar 10 numeros*/
+
+int main(int argc, char const *argv[])
+{
+    int i = 1;
+    while (i <= 9){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
